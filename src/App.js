aa1362fd4f952ee5103307f617c8c9387200f9f9
Aaftab K. Ansari <68555868+aaftab-ansari-15@users.noreply.github.com>
2024-11-06 10:23:38 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useSelector } from "react-redux";
 
-
 function App() {
-  const darkMode = useSelector((state) => state.modal.darkMode)
+  const darkMode = useSelector((state) => state.modal.darkMode);
   const darkTheme = createTheme({
     palette: {
       mode: darkMode ? "dark" : "light",
