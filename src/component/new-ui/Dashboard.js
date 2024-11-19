3fcf9c5ef66bfb5c202777afc42c34d2be77e569
aaftab.ansari <aaftab.ansari@crestinfotech.co.in>
2024-11-19 15:55:55 +0530

    changes before pull

--- src/component/new-ui/Dashboard.js
+++ src/component/new-ui/Dashboard.js
@@ -2,16 +2,7 @@ import { Box } from "@mui/material";
 import React from "react";
 
 const Dashboard = () => {
-  return (
-    <Box
-      sx={{
-        border: `solid 2px primary.main`, // Use primary color from theme
-        m: 2,
-        padding: 2,
-        color: "text.primary",
-      }}
-    >Dashboard</Box>
-  );
+  return <Box sx={{}}>Dashboard</Box>;
 };
 
 export default Dashboard;
