36e2d64b4c7d190debc976da27ea89c96105c694
aaftab.ansari <aaftab.ansari@crestinfotech.co.in>
2024-12-02 20:31:13 +0530

    new changes

--- src/component/features/CalendarComp.js
+++ src/component/features/CalendarComp.js
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from "react";
-import Calendar from "react-calendar";
-import { useDispatch } from "react-redux";
-import { changePickUpDate } from "../../redux/useFullSlice";
-const CalendarComp = () => {
-  const dispatch = useDispatch();
-  const [calDate, setCalDate] = useState(new Date());
-  useEffect(() => {
-    dispatch(changePickUpDate(calDate.toString()));
-  }, [calDate]);
+// import React, { useEffect, useState } from "react";
+// import Calendar from "react-calendar";
+// import { useDispatch } from "react-redux";
+// import { changePickUpDate } from "../../redux/useFullSlice";
+// const CalendarComp = () => {
+//   const dispatch = useDispatch();
+//   const [calDate, setCalDate] = useState(new Date());
+//   useEffect(() => {
+//     dispatch(changePickUpDate(calDate.toString()));
+//   }, [calDate]);
 
-  const onChange = (calDate) => {
-    // change results based on calendar date click
-    setCalDate(calDate);
-  };
+//   const onChange = (calDate) => {
+//     // change results based on calendar date click
+//     setCalDate(calDate);
+//   };
 
-  return <Calendar onChange={onChange} value={calDate} />;
-};
+//   return <Calendar onChange={onChange} value={calDate} />;
+// };
 
-export default CalendarComp;
+// export default CalendarComp;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-//
-// ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <RouterProvider router={router} /> */}
       <App />
     </Provider>
   </React.StrictMode>
